bb516f03dc1a1a2daf47fb5a9af27b0a982afae1
zhixuan <jinzhixuan666@gmail.com>
2022-09-18 02:07:29 +0000

    add disconnection hook add defult Koma

--- server/index.ts
+++ server/index.ts
@@ -38,13 +38,32 @@ type ioKoma = {
     isGoal: boolean;
 };
 
+const DefaultKoma: ioKoma[] = [
+    { owner: 0, id: 0, position: 14, isGoal: false },
+    { owner: 0, id: 1, position: 15, isGoal: false },
+    { owner: 0, id: 2, position: 16, isGoal: false },
+    { owner: 0, id: 3, position: 17, isGoal: false },
+    { owner: 1, id: 4, position: 32, isGoal: false },
+    { owner: 1, id: 5, position: 33, isGoal: false },
+    { owner: 1, id: 6, position: 34, isGoal: false },
+    { owner: 1, id: 7, position: 35, isGoal: false },
+    { owner: 2, id: 8, position: 50, isGoal: false },
+    { owner: 2, id: 9, position: 51, isGoal: false },
+    { owner: 2, id: 10, position: 52, isGoal: false },
+    { owner: 2, id: 11, position: 53, isGoal: false },
+    { owner: 3, id: 12, position: 68, isGoal: false },
+    { owner: 3, id: 13, position: 69, isGoal: false },
+    { owner: 3, id: 14, position: 70, isGoal: false },
+    { owner: 3, id: 15, position: 71, isGoal: false },
+];
+
 const rooms: ioGame[] = [];
 
 rooms.push({
     id: "1",
     name: "room1",
     users: [],
-    koma: [],
+    koma: DefaultKoma,
     nowUser: null,
 });
 
@@ -81,6 +100,20 @@ io.on("connection", (socket) => {
     };
     socket.emit("message", "Hello there!");
     io.to("room1").emit("update", rooms[0]);
+
+    socket.on("disconnect", () => {
+        socket.leave("room1");
+        rooms[0] = {
+            ...rooms[0],
+            users: rooms[0].users.filter((user) => user.socketID != socket.id),
+            nowUser:
+                rooms[0].nowUser?.socketID == socket.id
+                    ? rooms[0].users[0]
+                    : rooms[0].nowUser,
+        };
+        io.to("room1").emit("update", rooms[0]);
+        console.log("user disconnected");
+    });
 });
 
 io.on("disconnect", (socket) => {
