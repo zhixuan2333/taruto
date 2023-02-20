44dcd2f83aac820693b5c220a7e739e5e5c2f1d9
zhixuan2333 <zhixuan@zhixuan.dev>
2023-02-20 02:58:36 +0000

    remove emoji

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ function App(): JSX.Element {
           {game.id !== 'lobby' ? <UserList g={game} socket={socket} /> : null}
           {game.id !== 'lobby' ? <Who g={game} socket={socket} /> : null}
           {game.id !== 'lobby' && game.nowState === 0 ? (
-            <LoadPage status={0} text='ほかのみんなまだかなぁ？🥱' />
+            <LoadPage status={0} text='ほかのみんなまだかなぁ？' />
           ) : null}
           {game.nowState === 200 ? <Win g={game} socket={socket} /> : null}
         </>
